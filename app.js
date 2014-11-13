7ca9ff3ceed8e5880497922536e2284458eda045
Kiran Rao <k.ranrao@gmail.com>
2014-11-13 18:05:58 +0530

    First pass at jquery string needed to create a new entry

--- app.js
+++ app.js
@@ -27,18 +27,23 @@ $(document).ready(function(){
     var index = streams.home.length - 1;
     while(index > displayedTweetCount){
       var tweet = streams.home[index];
+      
+      /* creates new tweet */
+
+      $("<div class='container tweet'><div class='row'><div class='tweet-profile col-xs-4 col-md-2'><img src='./img/shawndrost.jpg'><span>@sdizzy</span></div></div>").prependTo($feed);
+
+      
       var $tweet = $("<div class='tweet'></div>");
       $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at);
       $tweet.prependTo($feed);
       index -= 1;
     }
-    setTimeout(function(){
+    /*setTimeout(function(){
       displayTweets();
-    }, 1000); 
+    }, 1000); paused! */
     // Reruns to search for new tweets
     // Why does an error occur when interval is 0 (instantaneous)?
   }
   displayTweets();
   // Begins displayTweets loop
-
 });
