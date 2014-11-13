78acd614dfccfb86799c3078962ecf0d504bf93e
Kiran Rao <k.ranrao@gmail.com>
2014-11-13 18:39:08 +0530

    Tweets now generate with styling

--- app.js
+++ app.js
@@ -28,22 +28,16 @@ $(document).ready(function(){
     while(index > displayedTweetCount){
       var tweet = streams.home[index];
       
-      /* creates new tweet */
+      /* Creates new tweet */
+      $("<div class='container tweet'><div class='row'><div class='tweet-profile col-xs-4 col-md-2'><img src='./img/" + tweet.user + ".jpg'><span>@" + tweet.user + "</span></div><div class='tweet-update-group col-xs-8 col-md-10'><div class='tweet-content'>" + tweet.message + "</div><div class='timestamp'><em>" + tweet.created_at + "</em></div></div></div></div>").prependTo($feed);
 
-      $("<div class='container tweet'><div class='row'><div class='tweet-profile col-xs-4 col-md-2'><img src='./img/shawndrost.jpg'><span>@sdizzy</span></div></div>").prependTo($feed);
-
-      
-      var $tweet = $("<div class='tweet'></div>");
-      $tweet.text('@' + tweet.user + ': ' + tweet.message + ' ' + tweet.created_at);
-      $tweet.prependTo($feed);
       index -= 1;
     }
-    /*setTimeout(function(){
+    setTimeout(function(){
       displayTweets();
-    }, 1000); paused! */
-    // Reruns to search for new tweets
+    }, 1000);
+    // Reruns every 1000 miliseconds to search for new tweets
     // Why does an error occur when interval is 0 (instantaneous)?
   }
   displayTweets();
-  // Begins displayTweets loop
 });
