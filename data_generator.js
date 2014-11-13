8d10d789da7843d5d6ec599ba770bec4e135f757
Kiran Rao <k.ranrao@gmail.com>
2014-11-13 20:09:26 +0530

    Edited to allow visitor to post and removed visitor from randomaly generated posts

--- data_generator.js
+++ data_generator.js
@@ -3,71 +3,74 @@
  * You can safely leave this file untouched, and confine your changes to index.html.
  */
 
-$(document).ready(function(){
-  // set up data structures
-  window.streams = {};
-  streams.home = [];
-  streams.users = {};
-  streams.users.shawndrost = [];
-  streams.users.sharksforcheap = [];
-  streams.users.mracus = [];
-  streams.users.douglascalhoun = [];
-  window.users = Object.keys(streams.users);
 
-  // utility function for adding tweets to our data structures
-  var addTweet = function(newTweet){
-    var username = newTweet.user;
-    streams.users[username].push(newTweet);
-    streams.home.push(newTweet);
-  };
+// set up data structures
+window.streams = {};
+streams.home = [];
+streams.users = {};
+streams.users.shawndrost = [];
+streams.users.sharksforcheap = [];
+streams.users.mracus = [];
+streams.users.douglascalhoun = [];
+// added kranrao userstream
+streams.users.kranrao = [];
+window.users = Object.keys(streams.users);
 
-  // utility function
-  var randomElement = function(array){
-    var randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
-  };
+// utility function for adding tweets to our data structures
+var addTweet = function(newTweet){
+  var username = newTweet.user;
+  streams.users[username].push(newTweet);
+  streams.home.push(newTweet);
+};
 
-  // random tweet generator
-  var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
-  var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
-  var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
-  var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
-  var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
+// utility function
+var randomElement = function(array){
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
 
-  var randomMessage = function(){
-    return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
-  };
+// random tweet generator
+var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the president', 'that wizard', 'a ninja', 'a seedy old man'];
+var verbs = ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
+var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
+var nouns = ['cat', 'koolaid', 'system', 'city', 'worm', 'cloud', 'potato', 'money', 'way of life', 'belief system', 'security system', 'bad decision', 'future', 'life', 'pony', 'mind'];
+var tags = ['#techlife', '#burningman', '#sf', 'but only i know how', 'for real', '#sxsw', '#ballin', '#omg', '#yolo', '#magic', '', '', '', ''];
 
-  // generate random tweets on a random schedule
-  var generateRandomTweet = function(){
-    var tweet = {};
-    tweet.user = randomElement(users);
-    tweet.message = randomMessage();
-    tweet.created_at = new Date();
-    addTweet(tweet);
-  };
+var randomMessage = function(){
+  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
+};
 
-  for(var i = 0; i < 10; i++){
-    generateRandomTweet();
+// generate random tweets on a random schedule
+var generateRandomTweet = function(){
+  var tweet = {};
+  tweet.user = randomElement(users);
+  tweet.message = randomMessage();
+  tweet.created_at = new Date();
+  if(tweet.user === 'kranrao'){
+    // do nothing
+  } else {
+    addTweet(tweet);}
+};
+
+for(var i = 0; i < 10; i++){
+  generateRandomTweet();
+}
+
+var scheduleNextTweet = function(){
+  generateRandomTweet();
+  setTimeout(scheduleNextTweet, Math.random() * 1500);
+};
+scheduleNextTweet();
+
+// utility function for letting students add "write a tweet" functionality
+// (note: not used by the rest of this file.)
+var writeTweet = function(message){
+  if(!visitor){
+    throw new Error('set the global visitor property!');
   }
-
-  var scheduleNextTweet = function(){
-    generateRandomTweet();
-    setTimeout(scheduleNextTweet, Math.random() * 1500);
-  };
-  scheduleNextTweet();
-
-  // utility function for letting students add "write a tweet" functionality
-  // (note: not used by the rest of this file.)
-  var writeTweet = function(message){
-    if(!visitor){
-      throw new Error('set the global visitor property!');
-    }
-    var tweet = {};
-    tweet.user = visitor;
-    tweet.message = message;
-    addTweet(tweet);
-  };
-
-});
+  var tweet = {};
+  tweet.user = visitor;
+  tweet.message = message;
+  addTweet(tweet);
+};
   
